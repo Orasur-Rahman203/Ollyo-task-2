d0aae5aa599880468fe71b4e93c9eb96b4dbfc23
Orasur-Rahman203 <orasurrahman203@gmail.com>
2023-11-05 22:53:28 +0600

    UP

--- src/imageComponent.jsx
+++ src/imageComponent.jsx
@@ -1,39 +1,45 @@
 import { useSortable } from '@dnd-kit/sortable'
 import React from 'react';
-import {CSS} from '@dnd-kit/utilities'
+import { CSS } from '@dnd-kit/utilities'
 
-function imageComponent({image}) {
-    // console.log(image)
-
-const {
+function imageComponent({ image }) {
+  // console.log(image)
+  const {
     attributes,
     listeners,
     setNodeRef,
     transform,
     transition
-}=useSortable({
-    id:image.id
-})
- 
-const style={
-    transform:CSS.Transform.toString(transform),
+  } = useSortable({
+    id: image.id
+  })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
     transition
-}
+  }
+
+  const handleSelected = (item) => {
+    console.log("handleSelected", item);
+  }
+
 
   return (
-    <div 
-    style={style}
-    {...attributes}
-    {...listeners}
-    ref={setNodeRef}
-    className='p-4 rounded-md shadow-md text-black my-3 bg-white hover:opacity-25'>
-<div className="form-control">
-  <label className="label cursor-pointer">
-    {/* <span className="label-text">Remember me</span>  */}
-    <input type="checkbox" checked="checked" className="checkbox checkbox-success" />
-  </label>
-</div>
-      <img src={image.image} alt='image'/>
+    <div
+      style={style}
+      {...attributes}
+      {...listeners}
+      ref={setNodeRef}
+      className='p-4 rounded-md shadow-md text-black my-3 bg-white hover:opacity-25'>
+      <div onClick={() => handleSelected(image)}>
+        <div className="form-control">
+          <label className="label cursor-pointer">
+            <input
+              type="checkbox" unchecked="unchecked" className="checkbox checkbox-success" />
+          </label>
+        </div>
+      </div>
+      <img src={image.image} alt='image' />
     </div>
   )
 }
